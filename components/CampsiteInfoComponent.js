b830762a9a46d2df6ea534e2ae0098dd407a1b50
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-31 15:41:56 -0700

    Imported needed elements from react-native + comments.js. Added comments to state

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
 
 function RenderCampsite({campsite}) {
    
@@ -24,7 +25,8 @@ class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS
         };
     }
 
