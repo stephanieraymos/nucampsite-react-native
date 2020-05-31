bbbd01d5b217fcd16b7d0743a1564173d77ea4df
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-31 15:57:04 -0700

    Added RenderComment Component

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -20,6 +20,29 @@ function RenderCampsite({campsite}) {
     return <View />;
 }
 
+
+function RenderComment({comments}) {
+
+    const renderCommentItem = ({item}) => {
+        return(
+            <View style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{` -- ${item.author}, ${item.date}`}</Text>
+            </View>
+        )
+    };
+    return(
+        <Card title='Comments'>
+            <FlatList
+            data={comments}
+            renderItem={renderCommentItem}
+            keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+    )
+}
+
 class CampsiteInfo extends Component {
     
     constructor(props) {
