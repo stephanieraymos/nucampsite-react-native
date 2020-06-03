fd9d71029c60ceb2412025f3baec0dba268d35af
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-03 16:12:06 -0700

    Added Loading Component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
+import { ConfigureStore } from './redux/ConfigureStore';
 
 const store = ConfigureStore();
 
